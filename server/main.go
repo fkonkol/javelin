ad854180e03ad831ad0f06d6567bd7b6820a317b
Filip Konkol <kaenel1337@gmail.com>
2022-04-24 19:38:10 +0200

    Add health-check endpoint

--- server/main.go
+++ server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
 
 func main() {
 	fmt.Println("Up and running")
+
+	http.HandleFunc("/health", healthCheck)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
